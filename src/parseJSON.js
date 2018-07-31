e4ba9d0504c98f1e00fe6b4c44576021a48f3db8
tae-lim <taelim92@gmail.com>
2018-07-30 20:28:51 -0700

    Completed Numbers and Strings cases

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,43 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  var i = 0;
+
+  var parseController = function() {
+    if (isString(json[i])) {
+      return parseString();
+    } else if (isNumber(json[i])) {
+      return parseNumber();
+    }
+  };
+
+  var isString = function(char) {
+    return char === '"';
+  };
+
+  var isNumber = function(char) {
+    return !isNaN(char);
+  };
+
+  var parseString = function() {
+    var result = '';
+    while (json[i] !== '"') {
+      result += json[i];
+      i++;
+    }
+    return result;
+  };
+
+  var parseNumber = function() {
+    var result = '';
+    while (isNumber(json[i]) || json[i] === '.' || json[i] === '-') {
+      result += json[i];
+      i++;
+    }
+
+    return Number(result);
+  };
+
+  return parseController();
 };
+
