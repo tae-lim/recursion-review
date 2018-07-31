e1a40d9f9c12a7eee032a6f53aaa90eff5aedc78
tae-lim <taelim92@gmail.com>
2018-07-30 20:25:24 -0700

    Completed stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //example input: {'a': [], 'c': {}, 'b': true}
+  //example output: "{"a":[],"c":{},"b":true}"
+
+  //strings : return the string
+  //number : coerce to string and return
+  //boolean : coerce to string and return
+  //null:
+  //array : iterate and for each value call stringifyJSON on each
+  //objects: iterate through key/values and call stringifyJSON on each
+  //functions: skip it
+  //undefined: skip it
+  //wrap entire result in string
+
+
+  if (Array.isArray(obj)) {
+    var results = [];
+    for (var i = 0; i < obj.length; i++) {
+      results.push(stringifyJSON(obj[i]));
+    }
+
+    return '[' + results.join(',') + ']';
+  }
+
+  if (typeof obj === 'object' && obj) {
+    var results = [];
+    for (var key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        continue;
+      }
+      results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+
+    return '{' + results.join(',') + '}';
+  }
+
+  if (typeof obj === 'string') {
+    return '\"' + obj + '\"';
+  }
+
+  //base case for numbers and booleans
+  return '' + obj;
+
 };
